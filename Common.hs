9422e5cd5f4b163114a2311ec70f62fedc18f777
Joey Hess <joeyh@joeyh.name>
2016-11-18 12:59:05 -0400

    Fix build with recent versions of cabal and ghc.

--- Common.hs
+++ Common.hs
@@ -13,7 +13,6 @@ import Data.String.Utils as X hiding (join)
 import Data.Monoid as X
 
 import System.FilePath as X
-import System.Directory as X
 import System.IO as X hiding (FilePath)
 #ifndef mingw32_HOST_OS
 import System.Posix.IO as X hiding (createPipe)
@@ -25,12 +24,12 @@ import Utility.Exception as X
 import Utility.SafeCommand as X
 import Utility.Process as X
 import Utility.Path as X
-import Utility.Directory as X
 import Utility.Monad as X
 import Utility.Data as X
 import Utility.Applicative as X
 import Utility.FileSystemEncoding as X
 import Utility.PosixFiles as X hiding (fileSize)
 import Utility.FileSize as X
+import Utility.Directory as X
 
 import Utility.PartialPrelude as X
