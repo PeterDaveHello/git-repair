9d82f181d58f8538247a7efb96d1be16d131c158
Sean Whitton <spwhitton@spwhitton.name>
2017-07-25 17:10:10 -0700

    patch Common.hs to avoid duplicate import errors

--- Common.hs
+++ Common.hs
@@ -13,7 +13,6 @@ import Data.String.Utils as X hiding (join)
 import Data.Monoid as X
 
 import System.FilePath as X
-import System.Directory as X
 import System.IO as X hiding (FilePath)
 #ifndef mingw32_HOST_OS
 import System.Posix.IO as X hiding (createPipe)
@@ -25,7 +24,7 @@ import Utility.Exception as X
 import Utility.SafeCommand as X
 import Utility.Process as X
 import Utility.Path as X
-import Utility.Directory as X
+import Utility.Directory as X hiding (getFileSize)
 import Utility.Monad as X
 import Utility.Data as X
 import Utility.Applicative as X
