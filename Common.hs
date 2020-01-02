2468e746eb323248c1055c84dd2430e4a7b09b35
Sean Whitton <spwhitton@spwhitton.name>
2020-01-02 18:34:57 +0000

    drop remaining patch

--- Common.hs
+++ Common.hs
@@ -25,7 +25,7 @@ import Utility.Exception as X
 import Utility.SafeCommand as X
 import Utility.Process as X
 import Utility.Path as X
-import Utility.Directory as X hiding (getFileSize)
+import Utility.Directory as X
 import Utility.Monad as X
 import Utility.Data as X
 import Utility.Applicative as X
